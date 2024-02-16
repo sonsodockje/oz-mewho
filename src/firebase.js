dacd3d4f747f20cfc84c7f9e93047dae678d1190
sonsodockje <hanaroo0115@naver.com>
2024-02-16 23:41:01 +0900

    🎉 mewho 첫 시작

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3rU6eKYAJospGk9BG6qFRWBqPhHhEYkk",
+  authDomain: "mewho-hello.firebaseapp.com",
+  databaseURL: "https://mewho-hello-default-rtdb.firebaseio.com",
+  projectId: "mewho-hello",
+  storageBucket: "mewho-hello.appspot.com",
+  messagingSenderId: "62002604250",
+  appId: "1:62002604250:web:edfb5561895918b4ff5de3",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
